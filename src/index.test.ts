4f23cf9f47d9850cf0d90f0feab8480167b3f3e8
Dennis Collinson <dennis.collective@gmail.com>
2021-11-09 16:47:58 -0700

    Backfill tests and add configurable time

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,107 @@
-import { recoilHookRenderContext } from '~index.ts'
+import { recoilHookRenderContext, act } from './index'
+import { atom, AtomEffect, MutableSnapshot, useRecoilState, useRecoilValue } from 'recoil'
+import { useEffect } from 'react'
 
 describe('recoilHookRenderContext', () => {
-  it('works', () => {
-    recoilHookRenderContext()
+  const BASIC_ATOM = atom<string>({ key: 'testStringAtrom', default: 'DEFAULT' })
+
+  it('gives us a context that holds the same recoil state to get hook values', async () => {
+    const { getCurrentValue } = recoilHookRenderContext()
+    const result = await getCurrentValue(() => useRecoilState(BASIC_ATOM))
+    expect(result[0]).toEqual('DEFAULT')
+
+    act(() => result[1]('New Friends'))
+    const [newValue] = await getCurrentValue(() => useRecoilState(BASIC_ATOM))
+    expect(newValue).toEqual('New Friends')
+  })
+
+  it('sets the initial state if provided', async () => {
+    const initializeState = (snapshot: MutableSnapshot): void => {
+      snapshot.set(BASIC_ATOM, 'PARTYTIME')
+    }
+    const { getCurrentValue } = recoilHookRenderContext(initializeState)
+    const [val, set] = await getCurrentValue(() => useRecoilState(BASIC_ATOM))
+    expect(val).toEqual('PARTYTIME')
+
+    //Assert having an initial state doesn't blow away resets
+    act(() => set('HAPPY NEW NOW'))
+    const [newVal] = await getCurrentValue(() => useRecoilState(BASIC_ATOM))
+    expect(newVal).toEqual('HAPPY NEW NOW')
+  })
+
+  describe('for atoms with immediate async effects', () => {
+    const asyncEffect: AtomEffect<string> = ({ setSelf }) => {
+      setTimeout(() => setSelf('POST ASYNC SET DEFAULT'), 0)
+    }
+
+    const ASYNC_EFFECT_ATOM = atom<string>({
+      key: 'testAsyncEffectAtom',
+      default: 'DEFAULT',
+      effects_UNSTABLE: [asyncEffect],
+    })
+
+    it('Waits for async effects to run', async () => {
+      const { getCurrentValue } = recoilHookRenderContext()
+      const val = await getCurrentValue(() => useRecoilValue(ASYNC_EFFECT_ATOM))
+      expect(val).toEqual('POST ASYNC SET DEFAULT')
+    })
+  })
+
+  describe('for atoms with delayed async effects', () => {
+    const delayedEffect: AtomEffect<string> = ({ setSelf }) => {
+      setTimeout(() => setSelf('POST ASYNC SET DEFAULT'), 10)
+    }
+
+    const DELAYED_ASYNC_EFFECT = atom<string>({
+      key: 'testDelayedEffectAtom',
+      default: 'DEFAULT',
+      effects_UNSTABLE: [delayedEffect],
+    })
+
+    it('allows delayed state mutation', async () => {
+      const { getCurrentValue } = recoilHookRenderContext()
+      let val: string | undefined
+
+      await act(async () => {
+        val = await getCurrentValue(() => useRecoilValue(DELAYED_ASYNC_EFFECT))
+      })
+      expect(val).toEqual('DEFAULT')
+
+      await act(async () => {
+        await new Promise((r) => setTimeout(r, 11))
+        val = await getCurrentValue(() => useRecoilValue(DELAYED_ASYNC_EFFECT))
+      })
+      expect(val).toEqual('POST ASYNC SET DEFAULT')
+    })
+
+    describe('for custom hooks that use react effects', () => {
+      const useRecoilCustomHooks = () => {
+        const [value, set] = useRecoilState(BASIC_ATOM)
+        useEffect(() => {
+          setTimeout(() => set('NEW VAL SET BY HOOK EFFECT'), 3)
+        }, [])
+
+        return value
+      }
+
+      it('allows you to use effects', async () => {
+        const { getCurrentValue } = recoilHookRenderContext()
+        let val: string | undefined
+        await act(async () => {
+          val = await getCurrentValue(useRecoilCustomHooks)
+          expect(val).toEqual('DEFAULT')
+          await new Promise((r) => setTimeout(r, 5))
+        })
+        val = await getCurrentValue(useRecoilCustomHooks)
+        expect(val).toEqual('NEW VAL SET BY HOOK EFFECT')
+      })
+
+      it('accepts a configurable timeout to await async effects', async () => {
+        const { getCurrentValue } = recoilHookRenderContext()
+
+        const val = await getCurrentValue(useRecoilCustomHooks, { timeout: 8 })
+        expect(val).toEqual('NEW VAL SET BY HOOK EFFECT')
+      })
+    })
   })
 })
---
+++ src/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import {
+  MutableSnapshot,
+  RecoilRoot,
+  useRecoilBridgeAcrossReactRoots_UNSTABLE as useRecoilBridge,
+} from 'recoil'
+import { act, renderHook, RenderHookResult } from '@testing-library/react-hooks'
+
+interface HookRenderOptions {
+  timeout?: number
+}
+interface RecoilHookRenderer {
+  renderRecoil<R>(hook: () => R, opts?: HookRenderOptions): Promise<RenderHookResult<undefined, R>>
+  getCurrentValue<R>(hook: () => R, opts?: HookRenderOptions): Promise<R>
+}
+
+function recoilHookRenderContext(initializeState?: (s: MutableSnapshot) => void): RecoilHookRenderer {
+  const { result: renderedBridge } = renderHook(useRecoilBridge, {
+    wrapper: ({ children }) => <RecoilRoot initializeState={initializeState}>{children}</RecoilRoot>,
+  })
+
+  const Bridge = renderedBridge.current
+  return bridgedHookRenderer(Bridge)
+}
+
+function bridgedHookRenderer(Bridge: React.FC) {
+  async function renderRecoil<R>(
+    hook: () => R,
+    opts?: HookRenderOptions
+  ): Promise<RenderHookResult<undefined, R>> {
+    const { timeout = 0 } = opts ?? {}
+
+    let rendered: RenderHookResult<undefined, R> | undefined
+
+    await act(async () => {
+      rendered = renderHook(hook, { wrapper: Bridge })
+      await new Promise((r) => setTimeout(r, timeout))
+    })
+
+    if (!rendered) throw new Error('Did not set hook result')
+
+    if (rendered.result.error) throw rendered.result.error
+    return rendered
+  }
+
+  async function getCurrentValue<R>(hook: () => R, opts?: HookRenderOptions): Promise<R> {
+    const { result } = await renderRecoil(hook, opts)
+    return result.current
+  }
+
+  return { renderRecoil, getCurrentValue }
+}
+
+export { act, recoilHookRenderContext }
